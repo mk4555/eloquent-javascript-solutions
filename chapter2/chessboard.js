2a33b2f729a6575d47fc64b67efc9f508a3090b2
Min Suk Kim <minkimcodes@gmail.com>
2018-08-02 16:14:50 -0400

    3rd solution to exercise added

---
+++ chapter2/chessboard.js
@@ -0,0 +1,16 @@
+function chessboard(size) {
+  let board = "";
+  for (let i=0; i<size; i++) {
+    for (let j=0; j<size; j++) {
+      if ((i + j) % 2 == 0) {
+        board += " ";
+      } else {
+        board += "#";
+      }
+    }
+    board += "\n";
+  }
+  console.log(board);
+}
+
+chessboard(8);
