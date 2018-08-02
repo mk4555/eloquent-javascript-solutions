879f16146f9b6fd51eed8e61357e56f214aec991
Min Suk Kim <minkimcodes@gmail.com>
2018-08-02 15:46:39 -0400

    second solution to exercise added

---
+++ chapter2/fizzbuzz.js
@@ -0,0 +1,16 @@
+function fizzbuzz(n) {
+  for (let i=1; i<=n; i++) {
+    if (i % 3 == 0 && i % 5 !== 0) {
+      console.log("Fizz")
+    } else if (i % 5 == 0 && i % 3 !== 0) {
+      console.log("Buzz")
+    } else if (i % 3 == 0 && i % 5 == 0)  {
+      console.log("Fizzbuzz")
+    }
+    else {
+      console.log(i)
+    }
+  }
+}
+
+fizzbuzz(100);
