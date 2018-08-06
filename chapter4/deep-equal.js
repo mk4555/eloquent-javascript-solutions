944a942765112bb9236780bf49c0865ccb17d8aa
Min Suk Kim <minkimcodes@gmail.com>
2018-08-06 14:54:03 -0400

    deep-equal solution added

---
+++ chapter4/deep-equal.js
@@ -0,0 +1,19 @@
+function deepEqual(obj1, obj2) {
+  if (typeof(obj1) !== "object" && typeof(obj2) !== "object") {
+    return obj1 === obj2
+  }
+
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+    return false;
+  }
+
+  for (let key in obj1) {
+    if (!(key in obj2)) {
+      return false;
+    }
+    if (!deepEqual(obj1[key], obj2[key])) {
+      return false;
+    }
+  }
+  return true;
+}
